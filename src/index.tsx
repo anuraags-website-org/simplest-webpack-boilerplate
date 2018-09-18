54e5158510a8b63b804f6ceaf7650a3a16376145
Anuraag Sridhar <anuraag.sridhar@gmail.com>
2018-09-18 10:25:02 +1000

    Added css declaration file.

--- src/index.tsx
+++ src/index.tsx
@@ -8,7 +8,9 @@ import * as styles from './index.css';
 class App extends React.Component {
   render() {
     return (
-      <h1>Hello World!</h1>
+      <div className={styles.app}>
+        <h1>Hello World!</h1>
+      </div>
     );
   }
 }
